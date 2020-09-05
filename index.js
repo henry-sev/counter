da3b853dfe2ec1020a78dc185d5ceec61344554a
henry-sev <q2466351926@gmail.com>
2020-09-05 17:42:31 +0800

    counter 1

---
+++ index.js
@@ -0,0 +1,10 @@
+function add() {
+  let num = document.getElementById("number");
+  num.textContent++;
+}
+
+function subtract() {
+  let num = document.getElementById("number");
+  num.textContent--;
+}
+
